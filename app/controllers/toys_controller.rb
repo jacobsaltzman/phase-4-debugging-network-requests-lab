e642532a3802e1ab7eaef04e9d2269e89f1f61ee
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-11-14 15:18:25 -0800

    commit tree b59eadfd4a6bfb9dfb394316213421554c6225e7

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,14 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params) #plural changed to singular
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy #this line was missing
   end
 
   def destroy
